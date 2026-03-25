1f5aa4513a4f87fed845ee5bcb8836384016dcf5
swkima-popos <greyonth@gmail.com>
2026-03-25 20:50:39 +0900

    add: Claudeに接続できることを確認

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use rig::client::CompletionClient;
+use rig::completion::Prompt;
+use rig::providers::anthropic::{Client, completion::ANTHROPIC_VERSION_LATEST};
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let api_key = &std::env::var("ANTHROPIC_API_KEY")?;
+    let client = Client::builder()
+        .api_key(api_key)
+        .anthropic_version(ANTHROPIC_VERSION_LATEST)
+        .build()?;
+
+    let agent = client
+        .agent("claude-sonnet-4-6")
+        .preamble("You are an helpful agent.")
+        .build();
+
+    let response = agent.prompt("Hello!, who are you?").await?;
+
+    println!("{response}");
+
+    Ok(())
 }
