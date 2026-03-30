a09b8cdd82d90d1ce16adf37a612ed16dc0d91e8
swkima-popos <greyonth@gmail.com>
2026-03-31 01:11:14 +0900

    feat: システムプロンプトを構築及び管理する構造体SystemContextsを作成し、それに伴ってmain.rsをリファクタリング

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 mod memory;
+mod system_context;
 mod tool;
 mod util;
 
-use std::env;
 use std::io::{self, Write};
 
 use rig::client::CompletionClient;
@@ -10,9 +10,8 @@ use rig::completion::Completion;
 use rig::message::AssistantContent;
 use rig::providers::anthropic::{Client, completion::ANTHROPIC_VERSION_LATEST};
 use rig::tool::ToolSet;
+use system_context::SystemContexts;
 use tool::{bash::Bash, grep::Grep, grob::Grob, read::Read, write::FullWrite};
-
-const SYSTEM_PROMPT: &str = "You are a helpful chatbot.";
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
@@ -37,7 +36,7 @@ async fn main() -> Result<(), anyhow::Error> {
     main_tool.add_tool(Grob);
     main_tool.add_tool(FullWrite);
 
-    let workspace_root_folder = env::current_dir()?;
+    let mut system_prompt = SystemContexts::new();
 
     loop {
         print!("\nYou: ");
@@ -57,17 +56,14 @@ async fn main() -> Result<(), anyhow::Error> {
         }
         main_memory.push_user(input.as_str());
 
-        let working_directory = env::current_dir()?;
-        let environment_prompt = format!(
-            "<env>
-                Working directory: {working_directory:?}
-                Workspace root folder: {workspace_root_folder:?}
-            </env>"
-        );
-
         let agent = client
             .agent("claude-sonnet-4-6")
-            .preamble(format!("{}\n{}", SYSTEM_PROMPT, environment_prompt).as_str())
+            .preamble(
+                &system_prompt
+                    .update_working_dir()
+                    .reload_instruction()
+                    .prompt(),
+            )
             .tool(Bash)
             .tool(Read)
             .tool(Grep)
